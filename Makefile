144bf2764e29ea17549ddd050049b40c219a6c32
Roman Galeev <jamhedd@gmail.com>
2020-01-11 15:02:53 +0100

    Switch to label hash

--- Makefile
+++ Makefile
@@ -1,10 +1,5 @@
 .PHONY: packages sanity
 
-bump: packages
-	git add .
-	git commit -a -m "Update generated"
-	git push origin HEAD
-
 packages: sanity
 	for folder in $$(find . -type d -maxdepth 1 -not -name docs -not -name '.*'); do helm package $$folder -d docs; done
 	helm repo index docs --url https://jamhed.github.io/charts
--- bump.sh
+++ bump.sh
@@ -5,11 +5,9 @@ CHART=$(yq r $NAME/Chart.yaml version)
 CHARTMINOR=$(echo $CHART | sed -E 's|^.*\.([0-9]+)$|\1|')
 CHARTMAJOR=$(echo $CHART | sed -E 's|(.*)\.[0-9]+$|\1|')
 NEWCHART="$CHARTMAJOR.$((CHARTMINOR+1))"
-yq w -i $NAME/Chart.yaml appVersion $NEWVER
 yq w -i $NAME/Chart.yaml version $NEWCHART
-git commit $NAME/Chart.yaml -m "Bump chart version to app:v$NEWVER chart:$NEWCHART"
-make helm
+git commit $NAME/Chart.yaml -m "Bump chart:$NAME to $NEWCHART"
+make
 git add docs
-git commit docs -m "Bump chart release to v$NEWVER"
-git push --tags
+git commit docs -m "Add release chart:$NAME:$NEWCHART"
 git push
