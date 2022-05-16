cf5c5092bc7f428a1b2b477357f412d2ab3d92ce
Roman Galeev <jamhedd@gmail.com>
2022-05-16 23:26:17 +0200

    Update bump script to the latest yq

--- bump.sh
+++ bump.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -ae
 NAME=$1
 [ -z "$NAME" ] && (echo "Usage: $0 chart"; exit 1)
-CHART=$(yq r $NAME/Chart.yaml version)
+CHART=$(yq e .version $NAME/Chart.yaml)
 CHARTMINOR=$(echo $CHART | sed -E 's|^.*\.([0-9]+)$|\1|')
 CHARTMAJOR=$(echo $CHART | sed -E 's|(.*)\.[0-9]+$|\1|')
 NEWCHART="$CHARTMAJOR.$((CHARTMINOR+1))"
-yq w -i $NAME/Chart.yaml version $NEWCHART
+yq -i ".version=\"$NEWCHART\"" $NAME/Chart.yaml
 git commit $NAME/Chart.yaml -m "Bump chart:$NAME to $NEWCHART"
 helm package $NAME -d docs
 helm repo index docs --url https://jamhed.github.io/charts
